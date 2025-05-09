55cc9ab66c02ac4d89ae9186f921eec72f950257
Sneha-19 <19snehasingh97@gmail.com>
2025-05-10 02:56:43 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -4,6 +4,18 @@ const app = express();
 
 const PORT = 5000;
 
+app.get("/user", (req, res) => {
+    res.send({firstname: "Sneha", lastname: "Singh"})
+})
+
+app.post("/user", (req, res) => {
+    res.send("Data sent successfully")
+})
+
+app.delete("/user", (req,res) => {
+    res.send("Deleted the data")
+})
+
 app.use("/test", (req, res) => {
     res.send("This is a testing page")
 })
