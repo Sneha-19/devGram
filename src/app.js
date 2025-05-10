b7fef41b25f19ebe692e3474ef91ad3a28b43d63
Sneha-19 <19snehasingh97@gmail.com>
2025-05-11 01:50:37 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./utils/database")
-const User = require("./models/user")
+const User = require("./models/user");
+const user = require("./models/user");
 
 const app = express();
 
@@ -17,6 +18,30 @@ app.post("/signUp", async (req, res) => {
     } catch(err) {
         res.status(400).send("Error in signup:", err.message)
     }
+})
+
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const users = await User.find({emailId : userEmail});
+        if(users.length === 0){
+            res.status(400).send("User not found")
+        } else {
+            res.send(users)
+        }
+    } catch(err) {
+        res.status(400).send("Semething went wrong")
+    }
+})
+
+app.get("/feed", async (req,res) => {
+   try {
+   const users = await User.find({});
+   res.send(users)
+   } catch(err) {
+    res.status(400).send("Semething went wrong")
+   }
 })
 
 connectDB().then(() => {
