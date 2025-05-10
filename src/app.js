7239346ec073f87b1c0e81f73bf182b4d406ba9d
Sneha-19 <19snehasingh97@gmail.com>
2025-05-11 00:21:03 +0530

    Databse connection using mongoose

--- src/app.js
+++ src/app.js
@@ -1,48 +1,16 @@
 const express = require("express");
+const connectDB = require("./utils/database")
 
 const app = express();
 
-const {isAdmin, userAuth} = require("./middlewares/auth")
-
 const PORT = 5000;
 
-// app.get("/user/:userId", (req,res) => {
-//     console.log(req.params);
-//     res.send("Param example")
-// })
-
-// app.get("/user", (req,res) => {
-//     console.log(req.query);
-//     res.send("Query printed")
-//     //call url: localhost:5000/user?name=Sneha
-// })
-
-app.use("/admin" , isAdmin);
-
-app.use("/admin/fetchUsers", (req, res) => {
-    res.send("All users returned");
+connectDB().then(() => {
+    console.log("Database connected successfully...");
+    app.listen(PORT, function(){
+        console.log("Server is running on port: ", PORT)
+    })
+}).catch( (err) => {
+    console.log("Database couldn't be connected!")
 })
 
-app.get("/user", userAuth, (req, res) => {
-    res.send({firstname: "Sneha", lastname: "Singh"})
-})
-
-app.post("/user", userAuth, (req, res) => {
-    res.send("Data sent successfully")
-})
-
-app.delete("/user", userAuth, (req,res) => {
-    res.send("Deleted the data")
-})
-
-app.use("/test", (req, res) => {
-    res.send("This is a testing page")
-})
-
-app.use("/", (req, res) => {
-    res.send("Welcome to the DevGram!!!")
-})
-
-app.listen(PORT, function(){
-    console.log("Server is running on port: ", PORT)
-})
