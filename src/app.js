cbea5835b891d4d51d5b74b9656527886fbc0f93
Sneha-19 <19snehasingh97@gmail.com>
2025-05-11 00:51:33 +0530

    signup api - user added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const connectDB = require("./utils/database")
+const User = require("./models/user")
 
 const app = express();
 
 const PORT = 5000;
+
+app.post("/signup", async (req, res) => {
+
+    const userData = {
+        firstName: "Sneha",
+        lastName: "Singh",
+        emailId: "sneha@singh.com",
+        password: "sneha123"
+    };
+
+    const user = new User(userData);
+    await user.save()
+    res.send("SignUp completed")
+})
 
 connectDB().then(() => {
     console.log("Database connected successfully...");
