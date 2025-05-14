f1657bb2eb19f5d8ccb6aa38548b0078a08a5ba8
Sneha-19 <19snehasingh97@gmail.com>
2025-05-14 23:04:04 +0530

    Logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -58,4 +58,12 @@ authRouter.post("/login", async (req, res) => {
     }
 })
 
+//Logout
+authRouter.post("/logout", async (req,res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now())
+    });
+    res.send("Logged out successfully")
+})
+
 module.exports = authRouter;
