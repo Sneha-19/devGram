eadbd1b2936fb4778db45400fd7103807d0e9aa8
Sneha-19 <19snehasingh97@gmail.com>
2025-05-15 15:34:44 +0530

    Forgot Password API

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
-const {profileEditDataValidation} = require("../utils/validation");
+const { profileEditDataValidation } = require("../utils/validation");
+const authRouter = require("./auth");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const profileRouter = express.Router();
 
@@ -16,9 +19,9 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
 })
 
 //Edit user profile
-profileRouter.patch("/profile/edit", userAuth, async (req,res) => {
-    try{
-        if(!profileEditDataValidation(req)){
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+    try {
+        if (!profileEditDataValidation(req)) {
             throw new Error("Invalid Profile Information")
         }
 
@@ -28,9 +31,32 @@ profileRouter.patch("/profile/edit", userAuth, async (req,res) => {
         })
 
         await loggedInUser.save()
-        res.json({"message": `${loggedInUser.firstName} your profile is updated successfully`, "data": loggedInUser})
-    } catch(err) {
-        res.status(400).send("ERROR : "+ err.message);
+        res.json({ "message": `${loggedInUser.firstName} your profile is updated successfully`, "data": loggedInUser })
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message);
+    }
+})
+
+//Reset Password (Forgot Password)
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        const { password, checkPassword } = req.body;
+
+        if (password !== checkPassword) {
+            throw new Error("Passwords don not match!")
+        }
+
+        if (!validator.isStrongPassword(password)) {
+            throw new Error("Kindly provide a strong paasword")
+        }
+
+        const encryptedPassword = await bcrypt.hash(password, 10);
+        user.password = encryptedPassword;
+        await user.save();
+        res.send("Password updated successfully!")
+    } catch (err) {
+        res.status(400).send("ERROR : " + res.message)
     }
 })
 
