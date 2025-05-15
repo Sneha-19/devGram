0a80d6e254d6cdd9038438155aede012266d1fcb
Sneha-19 <19snehasingh97@gmail.com>
2025-05-16 02:18:59 +0530

    Feed API

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const userRouter = express.Router();
 const ConnectionRequests = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const SAFE_STRING = "firstName lastName age gender imageUrl about skills";
 
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
-    try{
+    try {
         const loggedInUser = req.user;
 
         const requestsReceived = await ConnectionRequests.find({
@@ -15,35 +16,68 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
         }).populate("fromUserId", SAFE_STRING);
 
         res.json({
-            message: "Requests Received", 
+            message: "Requests Received",
             data: requestsReceived
         })
-    } catch(err) {
+    } catch (err) {
         res.status(400).send("ERROR : " + err.message);
     }
 })
 
-userRouter.get("/user/connections", userAuth, async (req,res) => {
-    try{
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
         const loggedInUser = req.user;
 
         const connectionRequests = await ConnectionRequests.find({
-            $or : [
-                {fromUserId: loggedInUser._id, status: "accepted"},
-                {toUserId: loggedInUser._id, status: "accepted"}
+            $or: [
+                { fromUserId: loggedInUser._id, status: "accepted" },
+                { toUserId: loggedInUser._id, status: "accepted" }
             ]
         }).populate("fromUserId", SAFE_STRING).populate("toUserId", SAFE_STRING);
 
         const data = connectionRequests.map(row => {
-            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+            if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
                 return row.toUserId;
             }
             return row.fromUserId
         })
 
-        res.json({data})
-    } catch(err) {
+        res.json({ data })
+    } catch (err) {
         res.status(400).send("ERROR :" + err.message)
+    }
+})
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+    try {
+        const loggedInUser = req.user;
+
+        const connectedUsers = await ConnectionRequests.find({
+            $or: [
+                { fromUserId: loggedInUser._id },
+                { toUserId: loggedInUser._id }
+            ]
+        }).select("fromUserId toUserId");
+
+        const connectUsersSet = new Set();
+
+        connectedUsers.forEach((user) => {
+            connectUsersSet.add(user.fromUserId.toString());
+            connectUsersSet.add(user.toUserId.toString());
+        })
+
+        const nonConnectedUsers = await User.find({
+           $and : [
+            {_id : { $nin : Array.from(connectUsersSet)}},
+            {_id : {$ne : loggedInUser._id}}
+           ]
+        }).select(SAFE_STRING)
+
+        res.json({
+            data: nonConnectedUsers
+        })
+    } catch (err) {
+        res.status(400).send("ERROR : " + err.message)
     }
 })
 
