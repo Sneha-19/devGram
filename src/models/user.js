506fe46febfc63c65c277c93311c8cd2788683f4
Sneha-19 <19snehasingh97@gmail.com>
2025-05-23 18:22:47 +0530

    user schema updated

--- src/models/user.js
+++ src/models/user.js
@@ -40,7 +40,7 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: String,
         validate(value) {
-            if (!["male", "female", "others"].includes(value)) {
+            if (!["Male", "Female", "Others"].includes(value)) {
                 throw new Error("Invalid gender type")
             }
         }
