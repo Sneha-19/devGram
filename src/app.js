5c3d5cf5647e82f1a8b3487a829fa914b3c579bc
Sneha-19 <19snehasingh97@gmail.com>
2025-06-06 21:55:52 +0530

    Chat (socket.io)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const connectDB = require("./utils/database");
 const cookieParser = require("cookie-parser");
+const http = require("http");
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const chatRouter = require("./routes/chat");
+const initializeSocket = require("./utils/socket");
 const cors = require("cors");
 require('dotenv').config()
 require("./utils/cronjobs")
@@ -23,10 +26,14 @@ app.use("/" , authRouter);
 app.use("/" , profileRouter);
 app.use("/" , requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB().then(() => {
     console.log("Database connected successfully...");
-    app.listen(process.env.PORT, function () {
+    server.listen(process.env.PORT, function () {
         console.log("Server is running on port: ", process.env.PORT)
     })
 }).catch((err) => {
